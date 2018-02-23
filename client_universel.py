7eb223bc4b8b32dad99c3b70a709a2d9320e0a4d
VictorJosso <victor@josso.fr>
2018-02-23 15:23:21 +0100

    Portabilité vers Windows assurée par la modification du dictionnaire 'colours' (les couleurs sur la console ne sont pas gérées par Windows)

--- client_universel.py
+++ client_universel.py
@@ -24,32 +24,60 @@ import generate_random_passwords as r_pass
 hote = "127.0.0.1"
 port = 26281
 
-colours = {
-	"default"    :    "\033[0m",
-	# style
-	"bold"       :    "\033[1m",
-	"underline"  :    "\033[4m",
-	"blink"      :    "\033[5m",
-	"reverse"    :    "\033[7m",
-	"concealed"  :    "\033[8m",
-	# couleur texte
-	"black"      :    "\033[30m",
-	"red"        :    "\033[31m",
-	"green"      :    "\033[32m",
-	"yellow"     :    "\033[33m",
-	"blue"       :    "\033[34m",
-	"magenta"    :    "\033[35m",
-	"cyan"       :    "\033[36m",
-	"white"      :    "\033[37m",
-	# couleur fond
-	"on_black"   :    "\033[40m",
-	"on_red"     :    "\033[41m",
-	"on_green"   :    "\033[42m",
-	"on_yellow"  :    "\033[43m",
-	"on_blue"    :    "\033[44m",
-	"on_magenta" :    "\033[45m",
-	"on_cyan"    :    "\033[46m",
-	"on_white"   :    "\033[47m" }
+if  not sys.platform[:3] == "win":
+	colours = {
+		"default"    :    "\033[0m",
+		# style
+		"bold"       :    "\033[1m",
+		"underline"  :    "\033[4m",
+		"blink"      :    "\033[5m",
+		"reverse"    :    "\033[7m",
+		"concealed"  :    "\033[8m",
+		# couleur texte
+		"black"      :    "\033[30m",
+		"red"        :    "\033[31m",
+		"green"      :    "\033[32m",
+		"yellow"     :    "\033[33m",
+		"blue"       :    "\033[34m",
+		"magenta"    :    "\033[35m",
+		"cyan"       :    "\033[36m",
+		"white"      :    "\033[37m",
+		# couleur fond
+		"on_black"   :    "\033[40m",
+		"on_red"     :    "\033[41m",
+		"on_green"   :    "\033[42m",
+		"on_yellow"  :    "\033[43m",
+		"on_blue"    :    "\033[44m",
+		"on_magenta" :    "\033[45m",
+		"on_cyan"    :    "\033[46m",
+		"on_white"   :    "\033[47m" }
+else:
+	colours = {
+		"default"    :    "",
+		# style
+		"bold"       :    "",
+		"underline"  :    "",
+		"blink"      :    "",
+		"reverse"    :    "",
+		"concealed"  :    "",
+		# couleur texte
+		"black"      :    "",
+		"red"        :    "",
+		"green"      :    "",
+		"yellow"     :    "",
+		"blue"       :    "",
+		"magenta"    :    "",
+		"cyan"       :    "",
+		"white"      :    "",
+		# couleur fond
+		"on_black"   :    "",
+		"on_red"     :    "",
+		"on_green"   :    "",
+		"on_yellow"  :    "",
+		"on_blue"    :    "",
+		"on_magenta" :    "",
+		"on_cyan"    :    "",
+		"on_white"   :    "" }
 
 class Releve(threading.Thread):
     def __init__(self):
